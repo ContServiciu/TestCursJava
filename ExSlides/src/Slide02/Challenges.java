105e48c450d088b32976ad7a0ca480aaaa1d3f0d
MaFomedanu <constantinliviudaniel@yahoo.com>
2024-03-16 03:08:48 +0200

    created feature branch

--- ExSlides/src/Slide02/Challenges.java
+++ ExSlides/src/Slide02/Challenges.java
@@ -1,6 +1,11 @@
 package Slide02;
 
 public class Challenges {
+  // File/folders added to complete the
+  // "Project Initialization" and "Working with Branches" parts
+  // of the second section slides challenges of the Java Course
+
+  // To successfully complete these challenges, the folders/files may be changed/updated
   public static void main(String[] args) {
         
     }
